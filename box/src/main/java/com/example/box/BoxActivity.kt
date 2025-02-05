d897d0fd579eb98f2e8e08964859c74426d4fa25
choco5732 <choco5732@gmail.com>
2025-02-06 00:28:45 +0900

    Box 복습

--- box/src/main/java/com/example/box/BoxActivity.kt
+++ box/src/main/java/com/example/box/BoxActivity.kt
@@ -3,12 +3,16 @@ package com.example.box
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.box.ui.theme.BoxTheme
@@ -27,15 +31,14 @@ class BoxActivity : ComponentActivity() {
 
 @Composable
 fun BoxExample() {
-    Box(
-        modifier = Modifier.size(100.dp)
-    ) {
-        Text(
-            text = "Hello World",
-            modifier = Modifier.align(Alignment.Center)
-        )
-    }
-
+//    Box(
+//        modifier = Modifier.size(100.dp)
+//    ) {
+//        Text(
+//            text = "Hello World",
+//            modifier = Modifier.align(Alignment.TopEnd)
+//        )
+//    }
 
     // 스텝 1 : Text 두 개를 Box 안에 배치하라
 //    Box(
@@ -47,36 +50,30 @@ fun BoxExample() {
 //        )
 //        Text(
 //            text = "존버는 승리한다",
-//            modifier = Modifier.align(Alignment.Center)
+//            modifier = Modifier.align(Alignment.CenterStart)
 //        )
 //        Text(
-//            text = "이곳에 모든걸 놓고왔다. 누려라.",
+//            text = "이곳에 모든걸 놓고왔다.",
 //            modifier = Modifier.align(Alignment.BottomEnd)
 //        )
 //    }
     // 스텝 2 : 2개의 Box를 Box안에 배치하고, 사이즈를 70dp, 색상을 각기 다르게 해보라.
 //    Box(
-//        modifier = Modifier.width(100.dp)
+//        modifier = Modifier.size(100.dp)
 //    ) {
 //        Box(
-//            modifier = Modifier
-//                .size(70.dp)
-//                .background(Color.Cyan)
-//                .align(Alignment.Center)
+//            modifier = Modifier.size(70.dp).background(Color.Cyan).align(Alignment.CenterStart)
+//        )
+//        Box(
+//            modifier = Modifier.size(70.dp).background(Color.Yellow).align(Alignment.BottomEnd)
 //        )
 //
-//        Box(
-//            modifier = Modifier
-//                .size(70.dp)
-//                .background(Color.Yellow)
-//                .align(Alignment.CenterEnd)
-//        )
 //    }
     // 스텝 3 : 부모 Box에 modifier 설정을 제거해서, 콘텐트 사이즈만큼 보여라.
-//    Box {
-//        Box(modifier = Modifier.fillMaxSize().background(Color.Cyan).align(Alignment.CenterStart))
-//        Box(modifier = Modifier.size(80.dp).background(Color.DarkGray).align(Alignment.Center))
-//    }
+    Box {
+        Box(modifier = Modifier.fillMaxSize().background(Color.Red).align(Alignment.CenterStart))
+        Box(modifier = Modifier.size(70.dp).background(Color.Yellow).align(Alignment.BottomEnd))
+    }
 }
 
 @Preview(showBackground = true)

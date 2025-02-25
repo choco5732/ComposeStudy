7468315195894451c7f676768ed6f39d063db8ad
choco5732 <choco5732@gmail.com>
2025-02-25 10:25:31 +0900

    dialog 학습

--- dialog/src/main/java/com/example/dialog/MainActivity.kt
+++ dialog/src/main/java/com/example/dialog/MainActivity.kt
@@ -28,7 +28,6 @@ class MainActivity : ComponentActivity() {
         // A surface container using the 'background' color from the theme
         setContent {
             ComposePracticeTheme {
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -48,23 +47,37 @@ class MainActivity : ComponentActivity() {
 
         Column {
             Button(onClick = { openDialog = !openDialog }) {
-                androidx.compose.material.Text("다이얼로그 열기")
+                Text("다이얼로그 열기")
             }
-            androidx.compose.material.Text("카운터: $counter")
+            Text("카운터: $counter")
         }
 
         if (openDialog) {
-            AlertDialog(onDismissRequest = {
+            AlertDialog(onDismissRequest = { // onDismissRequest 는 다이얼로그 외부를 클릭했을 때, 설정법
                 // 단계 1: `openDialog`를 이용해 다이얼로그를 끌 수 있게 합니다.
+                openDialog = false
             }, confirmButton = {
                 // 단계 2: "더하기" 버튼을 만들고 `counter`를 증가시킵니다.
                 // 다이얼로그도 끕니다.
+                Button(onClick = {
+                    counter ++
+                    openDialog = false
+                }) {
+                    Text("더하기")
+                }
             }, dismissButton = {
                 // 단계 3: "취소" 버튼을 만들고 다이얼로그를 끕니다.
+                Button(onClick = {
+                    openDialog = false
+                }) {
+                    Text("취소")
+                }
             }, title = {
                 // 단계 4: 타이틀을 만듭니다. "더하기" 정도로 해봅시다.
+                Text("더하기")
             }, text = {
                 // 단계 5: 다이얼로그에서 설명할 문구를 출력합니다.
+                Text("더하기 버튼을 누르면 카운터가 증가됩니다. \n버튼을 눌러주이소.")
             })
         }
     }

3e9352185b68991a4ef7b17cc89d530a1761d770
choco5732 <choco5732@gmail.com>
2025-02-25 09:12:05 +0900

    ConstraintLayout 활용 학습

---
+++ constraintlayout_advanced/src/main/java/com/example/constraintlayout_advanced/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.example.constraintlayout_advanced
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import coil.compose.AsyncImage
+import com.example.constraintlayout_advanced.ui.theme.ComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+    data class CardData(
+        val imageUri: String,
+        val imageDescription: String,
+        val author: String,
+        val description: String
+    )
+
+    companion object {
+        val cardData = CardData(
+            imageUri = "https://raw.githubusercontent.com/Fastcampus-Android-Lecture-Project-2023/part4-chapter3/main/part4-chapter3-10/app/src/main/res/drawable-xhdpi/wall.jpg",
+            imageDescription = "엔텔로프 캐년",
+            author = "Dalinaum",
+            description = "엔텔로프 캐년은 죽기 전에 꼭 봐야할 절경으로 소개되었습니다."
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePracticeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        CardEx(cardData)
+                        CardEx(cardData)
+                        CardEx(cardData)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CardEx(cardData: MainActivity.CardData) {
+    val placeHolderColor = Color(0x33000000)
+    Card(
+        elevation = 8.dp,
+        modifier = Modifier.padding(4.dp)
+    ) {
+        ConstraintLayout(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            val (profileImage, author, description) = createRefs()
+            AsyncImage(
+                model = cardData.imageUri,
+                contentDescription = cardData.imageDescription,
+                contentScale = ContentScale.Crop,
+                placeholder = ColorPainter(color = placeHolderColor),
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .constrainAs(profileImage) {
+//                        top.linkTo(parent.top)
+//                        bottom.linkTo(parent.bottom)
+                        centerVerticallyTo(parent)
+//                        linkTo(parent.top, parent.bottom)
+                        start.linkTo(parent.start, margin = 8.dp)
+                    }
+            )
+            Text(
+                text = cardData.author,
+                modifier = Modifier.constrainAs(author) {
+//                    start.linkTo(profileImage.end)
+                    linkTo(
+                        profileImage.end,
+                        parent.end,
+                        startMargin = 8.dp,
+                        endMargin = 8.dp
+                    )
+                    width = Dimension.fillToConstraints
+                }
+            )
+            Text(
+                text = cardData.description,
+                modifier = Modifier.constrainAs(description) {
+                    linkTo(
+                        profileImage.end,
+                        parent.end,
+                        startMargin = 8.dp,
+                        endMargin = 8.dp
+                    )
+                    width = Dimension.fillToConstraints
+                }
+            )
+            val chain = createVerticalChain(
+                author,
+                description,
+                chainStyle = ChainStyle.Packed
+            )
+            constrain(chain) {
+                top.linkTo(parent.top, margin = 8.dp)
+                bottom.linkTo(parent.bottom, margin = 8.dp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposePracticeTheme {
+        CardEx(cardData = MainActivity.cardData)
+    }
+}

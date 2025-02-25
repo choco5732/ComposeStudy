c40f777f87f1c05430653f39a55049cb3e9464d9
choco5732 <choco5732@gmail.com>
2025-02-25 10:59:33 +0900

    드롭다운메뉴 세팅

---
+++ dropdownmenu/src/main/java/com/example/dropdownmenu/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.dropdownmenu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dropdownmenu.ui.theme.ComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DropDownMenuEx()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DropDownMenuEx() {
+    var expandDropDownMenu by remember { mutableStateOf(false) }
+    var counter by remember { mutableStateOf(0) }
+
+    Column {
+        Button(onClick = { expandDropDownMenu = true }) {
+            Text("드롭다운 메뉴 열기")
+        }
+        Text("카운터: $counter")
+    }
+
+    // 단계 1: `DropdownMenu`를 만들고 `expanded`를 `expandDropDownMenu`로
+    // 등록합시다.
+    // `onDismissRequest`에 대해서는 `expandDropDownMenu`를 `false`로 바꿉니다.
+
+    // 단계 2: 두개의 `DropdownMenuItem`을 등록합시다. `onClick`을 구현하고
+    // 내용물은 `Text`로 채워봅시다.
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposePracticeTheme {
+        DropDownMenuEx()
+    }
+}
